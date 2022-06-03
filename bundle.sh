9a5f620e64f9198ba5653873cbd2a90a616bcde7
Jon Hultz <jhultz@redhat.com>
2022-06-03 13:34:18 -0700

    update

--- bundle.sh
+++ bundle.sh
@@ -2,6 +2,8 @@
 
 podman save -o $HOME/utility/tar-bundles/utility.tar localhost/utility:latest
 
-podman exec -d utility  oc-mirror ansible-playbook /root/utility/roles/utility-bundle/site.yaml
+podman exec -d utility ansible-playbook /root/utility/roles/utility-bundle/site.yaml
 
-tar -czf $HOME/utility/tar-bundles/sparta-utility.tar.gz $HOME/utility/ansible-roles $HOME/utility/tar-bundles/utility.tar
+tar -czf $HOME/utility/tar-bundles/sparta-utility.tar.gz $HOME/utility/ansible-roles $HOME/utility/tar-bundles/utility.tar $HOME/utility/tar-bundles/mirror-registry.tar.gz $HOME/utility/tar-bundles/oc-mirror.tar.gz $HOME/utility/tar-bundles/openshift-client-linux.tar.gz $HOME/utility/tar-bundles/openshift-install-linux.tar.gz
+
+rm -rf $HOME/utility/ansible-roles $HOME/utility/tar-bundles/utility.tar $HOME/utility/tar-bundles/mirror-registry.tar.gz $HOME/utility/tar-bundles/oc-mirror.tar.gz $HOME/utility/tar-bundles/openshift-client-linux.tar.gz $HOME/utility/tar-bundles/openshift-install-linux.tar.gz
