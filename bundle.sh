69fd80579ec951e0a02f5944ec34866a25cc951b
Jon Hultz <jhultz@redhat.com>
2022-06-03 13:24:34 -0700

    update

--- bundle.sh
+++ bundle.sh
@@ -4,4 +4,4 @@ podman save -o ${HOME}/utility/tar-bundles/utility.tar localhost/utility:latest
 
 podman exec -d utility  oc-mirror ansible-playbook /root/utility/roles/utility-bundle/site.yaml
 
-tar -czf ${HOME}/tar-bundles/sparta-utility.tar.gz ${HOME}/utility/ansible-roles ${HOME}/tar-bundles/utility.tar
+tar -czf ${HOME}/utility/tar-bundles/sparta-utility.tar.gz ${HOME}/utility/ansible-roles ${HOME}/utility/tar-bundles/utility.tar
