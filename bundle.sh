41ab4112b634605b4813cb78725315088546f21a
Jon Hultz <jhultz@redhat.com>
2022-06-06 13:51:20 -0700

    update

--- bundle.sh
+++ bundle.sh
@@ -4,6 +4,20 @@ podman save -o $HOME/utility/tar-bundles/utility.tar localhost/utility:latest
 
 podman exec utility ansible-playbook /root/utility/roles/utility-bundle/site.yaml
 
-tar -czf tar-bundles/sparta-utility.tar.gz ansible-roles tar-bundles/utility.tar tar-bundles/mirror-registry.tar.gz tar-bundles/oc-mirror.tar.gz tar-bundles/openshift-client-linux.tar.gz tar-bundles/openshift-install-linux.tar.gz tar-bundles/openshift-baremetal-install
+tar -czf $HOME/utility/tar-bundles/sparta-utility.tar.gz \
+$HOME/utility/ansible-roles \
+$HOME/utility/tar-bundles/utility.tar \
+$HOME/utility/tar-bundles/mirror-registry.tar.gz \
+$HOME/utility/tar-bundles/oc-mirror.tar.gz \
+$HOME/utility/tar-bundles/openshift-client-linux.tar.gz \
+$HOME/utility/tar-bundles/openshift-install-linux.tar.gz \
+$HOME/utility/tar-bundles/openshift-baremetal-install
 
-rm -rf $HOME/utility/ansible-roles/registry $HOME/utility/ansible-roles/ocp-install-build-config $HOME/utility/tar-bundles/utility.tar $HOME/utility/tar-bundles/mirror-registry.tar.gz $HOME/utility/tar-bundles/oc-mirror.tar.gz $HOME/utility/tar-bundles/openshift-client-linux.tar.gz $HOME/utility/tar-bundles/openshift-install-linux.tar.gz HOME/utility/tar-bundles/openshift-baremtal-install
+rm -rf $HOME/utility/ansible-roles/registry \
+$HOME/utility/ansible-roles/ocp-install-build-config \
+$HOME/utility/tar-bundles/utility.tar \
+$HOME/utility/tar-bundles/mirror-registry.tar.gz \
+$HOME/utility/tar-bundles/oc-mirror.tar.gz \
+$HOME/utility/tar-bundles/openshift-client-linux.tar.gz \
+$HOME/utility/tar-bundles/openshift-install-linux.tar.gz \
+$HOME/utility/tar-bundles/openshift-baremetal-install
--- init.sh
+++ init.sh
@@ -6,7 +6,6 @@ echo "$REPLY" > $HOME/.docker/config.json
 #Obtain baremtal installer
 podman exec -d utility oc adm release extract --registry-config /root/.docker/config.json --command openshift-baremetal-install --to tar-bundles quay.io/openshift-release-dev/ocp-release@sha256:a546cd80eae8f94ea0779091e978a09ad47ea94f0769b153763881edb2f5056e
 
-
 #Run oc-mirror inside the container
 podman exec -d utility  oc-mirror --config=./imageset-config.yaml file://output
 #Check for output file
@@ -17,6 +16,6 @@ while [ ! -f ./output/mirror_seq* ] ; do echo "Waiting for oc-mirror to finish" 
 
 #Tar up files to move to high side
 echo "Archiving files to be moved"
-cp ./output/mirror_seq* ocp-images.tar
+cp ./output/mirror_seq* tar-bundles/ocp-images.tar
 if test -e ocp-images.tar ; then echo "Archive successful" ; else echo "Archive failed" ; fi
 echo "Now transfer ocp-images.tar to high side"
