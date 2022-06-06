db11aac2f1dbad637e2ddc0d2fdb6fbc2f1ae738
Jon Hultz <jhultz@redhat.com>
2022-06-06 13:58:20 -0700

    update

--- bundle.sh
+++ bundle.sh
@@ -4,14 +4,14 @@ podman save -o $HOME/utility/tar-bundles/utility.tar localhost/utility:latest
 
 podman exec utility ansible-playbook /root/utility/roles/utility-bundle/site.yaml
 
-tar -czf $HOME/utility/tar-bundles/sparta-utility.tar.gz \
-$HOME/utility/ansible-roles \
-$HOME/utility/tar-bundles/utility.tar \
-$HOME/utility/tar-bundles/mirror-registry.tar.gz \
-$HOME/utility/tar-bundles/oc-mirror.tar.gz \
-$HOME/utility/tar-bundles/openshift-client-linux.tar.gz \
-$HOME/utility/tar-bundles/openshift-install-linux.tar.gz \
-$HOME/utility/tar-bundles/openshift-baremetal-install
+tar -czf utility/tar-bundles/sparta-utility.tar.gz \
+utility/ansible-roles \
+utility/tar-bundles/utility.tar \
+utility/tar-bundles/mirror-registry.tar.gz \
+utility/tar-bundles/oc-mirror.tar.gz \
+utility/tar-bundles/openshift-client-linux.tar.gz \
+utility/tar-bundles/openshift-install-linux.tar.gz \
+utility/tar-bundles/openshift-baremetal-install
 
 rm -rf $HOME/utility/ansible-roles/registry \
 $HOME/utility/ansible-roles/ocp-install-build-config \
