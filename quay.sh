ac4ac7500d32002b511f30fdd02d21ddc29b6567
Jon Hultz <jhultz@redhat.com>
2022-06-07 11:24:56 -0700

    update

---
+++ quay.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "Do you have the quay pull secret (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  read -sp "Enter quay pull secret: "
+  echo "$REPLY" > $HOME/.docker/config.json
+  echo "Configured pull secret";
+else
+  echo "Please refer to https://codectl.io/docs/user-guide/pre-req/ documentation on Quay pull secrets. You can also nagivate to https://cloud.redhat.com/openshift/install/metal/user-provisioned for more information";
+fi
