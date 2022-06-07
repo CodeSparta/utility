0f882b491da17f97c9f95ab01cde8ea855d87dfb
Jon Hultz <jhultz@redhat.com>
2022-06-07 14:31:56 -0700

    update

--- bundle.sh
+++ bundle.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+#Obtain baremtal installer
+podman exec -d utility oc adm release extract --registry-config /root/.docker/config.json --command openshift-baremetal-install --to tar-bundles quay.io/openshift-release-dev/ocp-release@sha256:a546cd80eae8f94ea0779091e978a09ad47ea94f0769b153763881edb2f5056e
+
 podman save -o $HOME/utility/tar-bundles/utility.tar localhost/utility:latest
 
 podman exec utility ansible-playbook /root/utility/roles/utility-bundle/site.yaml
 
 tar -czf tar-bundles/sparta-utility.tar.gz \
 ansible-roles \
-sparta2 \ 
+sparta2 \
 tar-bundles/utility.tar \
 tar-bundles/mirror-registry.tar.gz \
 tar-bundles/openshift-client-linux.tar.gz \
